e50195f1f559be96e2473f3ce68144727ef79543
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-02-27 19:19:19 +0700

    update list

--- src/components/Lists/List.jsx
+++ src/components/Lists/List.jsx
@@ -1,16 +1,16 @@
 const names = [
-  "Dalary Powell",
-  "Bennett Lloyd",
-  "Emely Keller",
-  "Nico Navarro",
-  "Winter Larson",
-  "Rafael Bravo",
-  "Amoura Graham",
-  "Giovanni King",
-  "Victoria Hoover",
-  "Jaziel Raymond",
-  "Hadlee Brown",
-  "Elijah Hartman",
+  { id: 1, name: "Dalary Powell", professions: "Software Engineer" },
+  { id: 2, name: "Bennett Lloyd", professions: "Software Engineer" },
+  { id: 3, name: "Emely Keller", professions: "Software Engineer" },
+  { id: 4, name: "Nico Navarro", professions: "Software Engineer" },
+  { id: 5, name: "Winter Larson", professions: "Software Engineer" },
+  { id: 6, name: "Rafael Bravo", professions: "Game Developer" },
+  { id: 7, name: "Amoura Graham", professions: "Game Developer" },
+  { id: 8, name: "Giovanni King", professions: "Game Developer" },
+  { id: 9, name: "Victoria Hoover", professions: "Game Developer" },
+  { id: 10, name: "Jaziel Raymond", professions: "Game Developer" },
+  { id: 11, name: "Hadlee Brown", professions: "Game Developer" },
+  { id: 12, name: "Elijah Hartman", professions: "Game Developer" },
 ];
 
 const List = () => {
