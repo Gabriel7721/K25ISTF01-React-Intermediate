4bb42170b8bbbae309f38dd04aa31335baef567a
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-02-27 19:47:59 +0700

    update

--- src/components/Lists/List.jsx
+++ src/components/Lists/List.jsx
@@ -1,22 +1,46 @@
-const names = [
-  { id: 1, name: "Dalary Powell", professions: "Software Engineer" },
-  { id: 2, name: "Bennett Lloyd", professions: "Software Engineer" },
-  { id: 3, name: "Emely Keller", professions: "Software Engineer" },
-  { id: 4, name: "Nico Navarro", professions: "Software Engineer" },
-  { id: 5, name: "Winter Larson", professions: "Software Engineer" },
-  { id: 6, name: "Rafael Bravo", professions: "Game Developer" },
-  { id: 7, name: "Amoura Graham", professions: "Game Developer" },
-  { id: 8, name: "Giovanni King", professions: "Game Developer" },
-  { id: 9, name: "Victoria Hoover", professions: "Game Developer" },
-  { id: 10, name: "Jaziel Raymond", professions: "Game Developer" },
-  { id: 11, name: "Hadlee Brown", professions: "Game Developer" },
-  { id: 12, name: "Elijah Hartman", professions: "Game Developer" },
+import { useState } from "react";
+
+const professions = ["Software Engineer", "Game Developer"];
+const person = [
+  { id: 1, name: "Dalary Powell", professions: professions[0] },
+  { id: 2, name: "Bennett Lloyd", professions: professions[0] },
+  { id: 3, name: "Emely Keller", professions: professions[0] },
+  { id: 4, name: "Nico Navarro", professions: professions[0] },
+  { id: 5, name: "Winter Larson", professions: professions[0] },
+  { id: 6, name: "Rafael Bravo", professions: professions[1] },
+  { id: 7, name: "Amoura Graham", professions: professions[1] },
+  { id: 8, name: "Giovanni King", professions: professions[1] },
+  { id: 9, name: "Victoria Hoover", professions: professions[1] },
+  { id: 10, name: "Jaziel Raymond", professions: professions[1] },
+  { id: 11, name: "Hadlee Brown", professions: professions[1] },
+  { id: 12, name: "Elijah Hartman", professions: professions[1] },
 ];
 
 const List = () => {
-  const listItems = names.map((n) => <li>{n}</li>);
+  const [selectedProf, setSelectedProf] = useState(professions[0]);
 
-  return <ul>{listItems}</ul>;
+  const filteredPerson = person.filter((p) => p.professions === selectedProf);
+
+  const listItems = filteredPerson.map((n) => (
+    <li key={n.id}>
+      {n.name} - {n.professions}
+    </li>
+  ));
+
+  return (
+    <>
+      <select
+        value={selectedProf}
+        onChange={(e) => setSelectedProf(e.target.value)}>
+        {professions.map((p) => (
+          <option key={p} value={p}>
+            {p}
+          </option>
+        ))}
+      </select>
+      <ol>{listItems}</ol>
+    </>
+  );
 };
 
 export default List;
